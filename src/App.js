180e52c2a2a497983ba1fa751a3a09944e0a681d
DESKTOP-2PR0O9I\big <you@example.com>
2022-12-16 18:31:13 +0200

    react router and links implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
-import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import FeedbackList from "./components/FeedbackList";
 import Header from "./components/Header";
 import { useState } from "react";
@@ -8,6 +8,7 @@ import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -22,19 +23,29 @@ function App() {
     setFeedback([newFeedback, ...feedback]);
   };
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-      </div>
-      <BrowserRouter>
         <Routes>
-          <Route path="/about">This is about ipage</Route>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<AboutPage />} />
         </Routes>
-      </BrowserRouter>
-    </>
+        <AboutIconLink />
+      </div>
+    </Router>
   );
 }
 
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import Card from "../components/shared/Card";
+import { Link } from "react-router-dom";
 function AboutPage() {
-  return <div>About</div>;
+  return (
+    <Card>
+      <div className="about">About</div>
+      <h1>This is app for giving feedback</h1>
+      <p>
+        <Link to="/">Back to Home page</Link>
+      </p>
+    </Card>
+  );
 }
 
 export default AboutPage;
